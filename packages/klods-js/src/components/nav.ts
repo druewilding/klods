341d6cba58cd18f28cefd5eee99df9673229161e
Drue Wilding <drue@wilding.dk>
2026-06-28 20:57:02 +0200

    feat: Enable TOC links to have an active property

--- packages/klods-js/src/components/nav.ts
+++ packages/klods-js/src/components/nav.ts
@@ -42,7 +42,13 @@ export function toggleNav(targetEl: HTMLElement): void {
 export type TocProps = { sub?: boolean };
 export const toc = builder<TocProps>({ tag: "ul", base: "klods-toc", modifiers: { sub: "klods-toc--sub" } });
 export const tocItem = tagBuilder("li");
-export const tocLink = tagBuilder("a");
+
+export type TocLinkProps = { active?: boolean };
+export const tocLink = builder<TocLinkProps>({
+  tag: "a",
+  base: "klods-toc__link",
+  modifiers: { active: "klods-toc__link--active" },
+});
 
 export type NavLinkProps = {
   href?: string;
